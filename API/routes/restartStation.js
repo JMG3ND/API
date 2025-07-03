11cffece1e87e67d0ce9e9575ee190e80399f1d1
Jose Miguel <jo0303se@hotmail.com>
2025-07-03 13:13:36 -0500

    Refactorizar la ruta de restartStation para simplificar el manejo de procesos

--- API/routes/restartStation.js
+++ API/routes/restartStation.js
@@ -1,24 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {
-  findProcess,
-  killProcess,
-  startProcess,
-} = require("../../functions/checkProcess");
+const { findProcess, killProcess } = require("../../functions/checkProcess");
 
-const simbaProcess = "Simbat.exe";
-const simbaPath = "C:\\Proyecto web\\API\\simbat.bat";
 const svrProcess = "SvrProcessor.exe";
-const svrPath = "C:\\Simbat\\Comm\\SvrProcessor.exe";
 
 router.get("/", async (req, res) => {
   try {
-    if (await findProcess(simbaProcess)) {
-      await killProcess(simbaProcess);
-
-      if (await findProcess(svrProcess)) await killProcess(svrProcess);
-    }
-    await startProcess(simbaPath, simbaProcess);
+    if (await findProcess(svrProcess)) await killProcess(svrProcess);
     res.status(200).send("Estación Reiniciada");
   } catch (error) {
     res.status(500).send("Error al reiniciar la estación");
